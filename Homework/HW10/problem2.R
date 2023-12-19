cf09008c7212dde445c79fd2d7454858af70a1d9
Valentino <e12223211@student.tuwien.ac.at>
2023-12-19 17:44:54 +0100

    small enhancement for problem 2 of HW10

--- Homework/HW10/problem2.R
+++ Homework/HW10/problem2.R
@@ -1,4 +1,6 @@
 n_students <- 137
 yes_voters <- 131
 
-prop.test(yes_voters, n_students, alternative = "greater", conf.level = 0.99)
+#prop.test(yes_voters, n_students, p = 3/4, alternative = "greater", conf.level = 0.99)
+
+prop.test(yes_voters, n_students, p = 3/4, alternative = "less", conf.level = 0.99)
